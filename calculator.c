6b845dc2e03be1bc1b2cc9f70317f8961ea3bfba
Codecode1297 <sonalissagar@gmail.com>
2019-12-05 06:20:23 -0800

    calculator committed

---
+++ calculator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a=10,b=20;
+printf("\n addition =%d",a+b);
+printf("\n subtraction =%d",b-a);
+printf("\n multi =%d",a*b);
+printf("\n div =%d",b/a);
+
+
+return 0;
+
+}
